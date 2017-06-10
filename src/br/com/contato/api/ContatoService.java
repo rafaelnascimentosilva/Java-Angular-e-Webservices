711362fd44ab7c90c835e62a8bc1c790c67b70a1
rafaelnascimentosilva <rafaelnasc.silv@gmail.com>
2017-06-10 14:51:41 -0300

    criando metodo deletar e atualizar na Api Java

--- src/br/com/contato/api/ContatoService.java
+++ src/br/com/contato/api/ContatoService.java
@@ -3,8 +3,10 @@ package br.com.contato.api;
 import java.text.ParseException;
 import java.util.List;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -16,25 +18,37 @@ import br.com.contato.dao.ContatoDAO;
 import br.com.contato.modelo.Contato;
 
 @Path("/contato")
+@Produces("application/json")
 public class ContatoService {
-	
+
 	Connection connection = (Connection) new ConnectionFactory().getConnecticon();
+
 	@GET
 	@Path("/contatos")
-	@Produces("application/json")
-	public List<Contato> contatos(){
+	public List<Contato> contatos() {
 		return new ContatoDAO(connection).getLista();
 	}
-	
+
 	@POST
-	public void Adiciona( Contato contato) throws ParseException{
-		 new ContatoDAO(connection).inserir(contato);		
+	public void Adiciona(Contato contato) throws ParseException {
+		new ContatoDAO(connection).inserir(contato);
 	}
-	
+
+	@DELETE
+	@Path("{id}")
+	public void Deleta(@PathParam("id") Integer id) throws ParseException {
+
+	}
+
+	@PUT
+	@Path("{id}")
+	public void Atualiza(@PathParam("id") Integer id, Contato contato) throws ParseException {
+
+	}
+
 	@GET
-	@Path("{id}")	
-	@Produces("application/json")
-	public Contato getContato(@PathParam("id") Integer id){
-		 return new ContatoDAO(connection).getContato(id);
+	@Path("{id}")
+	public Contato getContato(@PathParam("id") Integer id) {
+		return new ContatoDAO(connection).getContato(id);
 	}
 }
